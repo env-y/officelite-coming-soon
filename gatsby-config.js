97062ec130d45b189b2594ad5f03b63a63bfd067
env-y <marinabuylina@gmail.com>
2020-12-10 20:48:31 +0300

    add material ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-remark",
